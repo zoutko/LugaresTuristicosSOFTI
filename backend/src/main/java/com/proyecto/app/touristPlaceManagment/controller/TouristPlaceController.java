d2e5473254d281716eb4463979795a569cd0d2fb
zoutko <byan.toprez@gmail.com>
2026-05-21 08:35:28 -0500

    hotfix: added api to places endpoint

--- backend/src/main/java/com/proyecto/app/touristPlaceManagment/controller/TouristPlaceController.java
+++ backend/src/main/java/com/proyecto/app/touristPlaceManagment/controller/TouristPlaceController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/places")
+@RequestMapping("/api/places")
 public class TouristPlaceController {
 
     private final TouristPlaceService touristPlaceService;
